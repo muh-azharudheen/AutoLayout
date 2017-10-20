c96b718d11bd7f3aebb015ddd914df425cc12752
Mac <mac@Macs-MacBook-Pro.local>
2017-10-21 01:06:40 +0400

    Main Interface Completed

---
+++ AutoLayout/AutoLayout/Extensions/Colors.swift
@@ -0,0 +1,13 @@
+//
+//  Colors.swift
+//  AutoLayout
+//
+//  Created by Mac on 10/21/17.
+//  Copyright © 2017 muh_azharudheen. All rights reserved.
+//
+
+import UIKit
+extension UIColor{
+    static var blurPink = UIColor(red: 249/255, green: 207/255, blue: 224/255, alpha: 1)
+    static var mainPink = UIColor(red: 232/255, green: 68/255, blue: 133/255, alpha: 1)
+}
--- AutoLayout/AutoLayout/ViewController.swift
+++ AutoLayout/AutoLayout/ViewController.swift
@@ -9,17 +9,111 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let topImageContainerView : UIView = {
+        let v = UIView()
+        v.translatesAutoresizingMaskIntoConstraints = false
+        return v
+    }()
+    
+    private let bearImageView : UIImageView = {
+        let iv = UIImageView(image: #imageLiteral(resourceName: "bear_first"))
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        iv.contentMode = .scaleAspectFit
+        return iv
+    }()
+    
+    private let descriptionTextView : UITextView = {
+       let tv = UITextView()
+        tv.translatesAutoresizingMaskIntoConstraints = false
+        let attributedText = NSMutableAttributedString(string: "Join us today in our fun and games!", attributes: [.font:UIFont.boldSystemFont(ofSize: 18)])
+        attributedText.append(NSAttributedString(string: "\n\n\nAre you ready for loads and loads of fun?. Dont wait any longer!. we hope to see you in stores soon.", attributes: [.font: UIFont.systemFont(ofSize: 13), .foregroundColor: UIColor.lightGray]))
+        tv.attributedText = attributedText
+        tv.isScrollEnabled = false
+        tv.isEditable = false
+        tv.textAlignment = .center
+        return tv
+    }()
+    
+    private let bottomStackView : UIStackView = {
+        let sv = UIStackView()
+        sv.distribution = .fillEqually
+        sv.translatesAutoresizingMaskIntoConstraints = false
+        return sv
+    }()
+    
+    private let previousBtn : UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("PREV", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.gray, for: .normal)
+        return button
+    }()
+    
+    private let nextBtn : UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("NEXT", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.mainPink, for: .normal)
+        return button
+    }()
+    
+    private let pageControl : UIPageControl = {
+       let pc = UIPageControl()
+        pc.currentPage = 0
+        pc.numberOfPages = 4
+        pc.currentPageIndicatorTintColor = .mainPink
+        pc.pageIndicatorTintColor = .blurPink
+        return pc
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        setupViews()
+        setupBottomControls()
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    private func setupViews(){
+        
+        view.addSubview(topImageContainerView)
+        topImageContainerView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        topImageContainerView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        topImageContainerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
+        topImageContainerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5).isActive = true
+        
+        
+        topImageContainerView.addSubview(bearImageView)
+        bearImageView.centerXAnchor.constraint(equalTo: topImageContainerView.centerXAnchor).isActive = true
+        bearImageView.centerYAnchor.constraint(equalTo: topImageContainerView.centerYAnchor).isActive = true
+        bearImageView.heightAnchor.constraint(equalTo: topImageContainerView.heightAnchor, multiplier: 0.5).isActive = true
+        
+        view.addSubview(descriptionTextView)
+        
+        descriptionTextView.topAnchor.constraint(equalTo: topImageContainerView.bottomAnchor).isActive = true
+        descriptionTextView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 24).isActive = true
+        descriptionTextView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -24).isActive = true
+        descriptionTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
+        
     }
-
-
+    
+    fileprivate func setupBottomControls(){
+        view.addSubview(bottomStackView)
+        
+        bottomStackView.addArrangedSubview(previousBtn)
+        bottomStackView.addArrangedSubview(pageControl)
+        bottomStackView.addArrangedSubview(nextBtn)
+        
+        NSLayoutConstraint.activate([
+            bottomStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            bottomStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            bottomStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            bottomStackView.heightAnchor.constraint(equalToConstant: 50)
+            ])
+    }
 }
 
